aa9caf3df7be13b994f6081f3e3bc5fd426b2ae8
vijayasatya <vijayasatyad@gmail.com>
2022-04-24 23:10:14 +0530

    new commit

--- Deep_Learning/Compturer_Vision_Projects/Object_Detection/2022/Sign-Language-Generation-From-Video-using-YOLOV5/yolov5/run.py
+++ Deep_Learning/Compturer_Vision_Projects/Object_Detection/2022/Sign-Language-Generation-From-Video-using-YOLOV5/yolov5/run.py
@@ -1,3 +1,5 @@
 import os
+#Working command for object detection for images
+#python detect.py --weights best.pt --img 416 --conf 0.5 --source C:\Users\vijay\Desktop\Git_ML_Project_Codes\Machine_Learning\Deep_Learning\Compturer_Vision_Projects\Object_Detection\2022\Sign-Language-Generation-From-Video-using-YOLOV5\yolov5\data\images
 
 os.system("python detect.py --weights best.pt --img 416 --conf 0.5 --source 0")
